3397e2a8db01def09b96e162e29d9ac0cb69478d
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-06-09 14:25:21 +0300

    Lesson7

---
+++ src/main/java/Lesson7/Lesson7.java
@@ -0,0 +1,14 @@
+package Lesson7;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+        TestsTest TestsTest = new TestsTest();
+        Tests.start(TestsTest.getClass());
+        ClassExplorer.outClassInfo(String.class);
+    }
+}
+
+
+
+
+
